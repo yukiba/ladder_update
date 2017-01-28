50734e331a9a6537e6ae4809457bf1cd99e507b6
sjtudoit <songlian@SongLians-MacBook-Pro.local>
2017-01-28 20:34:06 +0800

    再删除一个和钉钉ticket相关的初始化文件

--- config/initializers/dingtalk.rb
+++
@@ -1,9 +0,0 @@
-Rails.configuration.to_prepare do
-
-  Dingtalk.corpid = ENV['SJTUDOIT_DINGTALK_CORPID']
-  Dingtalk.corpsecret = ENV['SJTUDOIT_DINGTALK_CORPSECRET']
-
-  if Dingtalk.corpid.nil? || Dingtalk.corpsecret.nil?
-    raise Exception, 'plz set ding talk key and secret'
-  end
-end
